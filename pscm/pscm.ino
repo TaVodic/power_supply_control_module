3f07ca7f4b04d536920fb1a93db2c9436cf4f8db
Martin Tavoda <martin.tavoda@gmail.com>
2026-02-01 21:54:50 +0100

    [SW] Encoder implementation, using INT0 & INT1

--- pscm/pscm.ino
+++ pscm/pscm.ino
@@ -1,11 +1,17 @@
+#include "Encoder.h"
+//#include "MCP4251.h"
+//#include "dispsniff.h"
+#include "pscm.h"
+
 #include <Arduino.h>
 #include <stdint.h>
-#include "dispsniff.h"
-#include "pscm.h"
-#include "MCP4251.h"
 
-MCP4251 MCP_1(pin_MCP_CS1);
-MCP4251 MCP_2(pin_MCP_CS2);
+//MCP4251 MCP_1(pin_MCP_CS1);
+//MCP4251 MCP_2(pin_MCP_CS2);
+Encoder ENC_V(3, 2);
+//Encoder ENC_C(pin_ENC_C_A, pin_ENC_C_B);
+
+int16_t oldPosition = 0;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -16,13 +22,13 @@ void setup() {
   Serial.println();
   Serial.println("TM1640 sniff start ");
 
-  dispsniff_begin();
-  MCP_1.begin();
-  MCP_2.begin();
+  //dispsniff_begin();
+  //MCP_1.begin();
+  //MCP_2.begin();
 }
 
 void loop() {
-  uint16_t voltage;
+  /*uint16_t voltage;
   uint16_t current;
   uint16_t power;
   if (dispsniff_poll(&voltage, &current, &power) == 2) {
@@ -32,5 +38,11 @@ void loop() {
     Serial.print(current);
     Serial.print("  W=");
     Serial.println(power);
+  }*/
+
+  long newPosition = ENC_V.read();
+  if (newPosition != oldPosition) {
+    oldPosition = newPosition;
+    Serial.println(newPosition);
   }
 }
