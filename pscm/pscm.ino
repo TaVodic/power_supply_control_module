ddcd84941d481d114a426aa6cbd184e261458724
Martin Tavoda <martin.tavoda@gmail.com>
2026-02-01 20:03:44 +0100

    [SW] MCP lib added

--- pscm/pscm.ino
+++ pscm/pscm.ino
@@ -1,7 +1,11 @@
-#include "dispsniff.h"
-#include "pscm.h"
 #include <Arduino.h>
 #include <stdint.h>
+#include "dispsniff.h"
+#include "pscm.h"
+#include "MCP4251.h"
+
+MCP4251 MCP_1(pin_MCP_CS1);
+MCP4251 MCP_2(pin_MCP_CS2);
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -13,6 +17,8 @@ void setup() {
   Serial.println("TM1640 sniff start ");
 
   dispsniff_begin();
+  MCP_1.begin();
+  MCP_2.begin();
 }
 
 void loop() {
