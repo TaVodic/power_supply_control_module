c7b0575e3a0c27e4dc2877c30fda6e1b726d415e
Martin Tavoda <martin.tavoda@gmail.com>
2026-02-11 14:39:00 +0100

    upload.sh updated for atmega328pb

--- pscm/upload.sh
+++ pscm/upload.sh
@@ -1,13 +1,20 @@
+arduino-cli compile -b MiniCore:avr:328 --libraries ./lib -v --output-dir ./build
+C:/Users/Martin/AppData/Local/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino17/bin/avrdude              \
+    -CC:/Users/Martin/AppData/Local/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino17/etc/avrdude.conf   \
+    -v -V -patmega328pb -cusbasp -Pusb -U lfuse:w:0xEF:m -Uflash:w:build/pscm.ino.hex:i
+
 #arduino-cli compile -b arduino:avr:nano -v --output-dir ./build
 #C:/Users/Martin/AppData/Local/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino17/bin/avrdude              \
 #    -CC:/Users/Martin/AppData/Local/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino17/etc/avrdude.conf   \
-#    -v -V -patmega328p -cusbasp -Pusb -Uflash:w:C:/Users/Martin/Documents/Arduino/BLink/build/Blink.ino.hex:i
+#    -v -V -patmega328p -cusbasp -Pusb -Uflash:w:build/pscm.ino.hex:i
 
-#C:/Users/Martin/AppData/Local/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino17/bin/avrdude              \
-#    -CC:/Users/Martin/AppData/Local/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino17/etc/avrdude.conf   \
-#    -v -V -patmega168p -cstk500v1 -PCOM9 -b19200 -U lfuse:w:0xDE:m -U hfuse:w:0xDF:m -U efuse:w:0xF9:m -Uflash:w:C:/Users/Martin/Documents/Arduino/LedStrip/build/LedStrip.ino.hex:i
-
-arduino-cli compile -b arduino:avr:uno --libraries ./lib -v --output-dir ./build
-arduino-cli upload -p COM10
+#arduino-cli compile -b arduino:avr:uno --libraries ./lib -v --output-dir ./build
+#arduino-cli upload -p COM10
 
 "/c/Users/Martin/AppData/Local/Arduino15/packages/arduino/tools/avr-gcc/7.3.0-atmel3.6.1-arduino7/bin/avr-objdump.exe" -d -S build/pscm.ino.elf > build/pscm.ino.elf.asm
+
+#CKSEL0 1
+#SUT1:0 10
+#CKSEL[3:1] 111
+
+#11101111
